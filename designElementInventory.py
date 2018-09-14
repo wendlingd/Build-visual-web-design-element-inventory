a67844679d1d269b7f61f51011f03bc52779bf9d
wendlingd <wendlingd@users.noreply.github.com>
2018-09-14 18:21:22 -0400

    first commit

---
+++ designElementInventory.py
@@ -0,0 +1,48 @@
+# ========================================
+# 7. Get screenshot for every URL in file
+# ========================================
+'''
+Required before running: Text file with one URL per line. I use a spider to 
+find elements on pages and traffic / site area to guide what subset of site 
+URLs to visit.
+
+What types of page elements you can target: https://selenium-python.readthedocs.io/locating-elements.html
+
+xpath examples:
+    /html/body/footer
+    //form[@id='loginForm']
+    //form[not(contains(@action, 'dont-want'))]
+    //form[(contains(@action, 'do-want'))]")
+    //button
+'''
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import os
+# from selenium.webdriver.chrome.options import Options
+
+# Set working directory
+os.chdir('/Users/user/Projects/DesignSystem/UrlScreenshotGrabber')
+
+# Name of the URL list to process
+sourceListFile = "siteFormUrls.txt"
+
+def save_screenshots(sourceList):
+    for index, url in enumerate(sourceList):
+        options = Options()
+        options.add_argument( "--headless" )
+        driver = webdriver.Firefox( firefox_options=options )
+        # driver = webdriver.Chrome('/Users/name/anaconda3/bin/chromedriver')
+        driver.get(url)
+        element = driver.find_element_by_xpath("//form")
+        element.location_once_scrolled_into_view
+        fname = url.replace("https://www.mysite.org/", "")
+        fname = fname.replace("/", "-")
+        fname = fname.replace(".", "-")
+        fname = fname.replace("\n", "")
+        driver.save_screenshot(fname + "-{}.png".format(index)) # Number protects because multiple elements may appear on one page
+        driver.quit()
+ 
+sourceList = open(sourceListFile, "r")
+ 
+save_screenshots(sourceList)
